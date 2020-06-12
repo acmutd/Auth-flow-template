26627acb268339877c9a7dcf6b8516a47b0b1a64
Harsha Srikara <harshasrikara@gmail.com>
2020-06-12 11:10:11 -0500

    test sample workflow

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
 
-import { Card, CardImg, CardBody, CardText, CardTitle, CardSubtitle } from "reactstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 class Content extends Component {
   render() {
     return (
